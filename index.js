0107adfdc23aad216a0c3887c6cbf4d52a3cc72c
younus35 <younuscode35@gmail.com>
2024-01-31 08:03:23 +0530

    added the get

--- index.js
+++ index.js
@@ -10,13 +10,30 @@ function handleFormSubmit(event){
        phone:phone
     };
     // let myobj_serialized = JSON.stringify(myobj);
-    //localStorage.setItem(email,myobj_serialized);
+    // localStorage.setItem(email,myobj_serialized);
+   
+      axios.get("https://crudcrud.com/api/9dc38fe5d920496db56c62c49eb6370d/AppointmentDetails")
+      .then((res) =>{
+        // console.log(res.data);
+  
+          for(var i=0; i<res.data.length; i++){
+                  showUserOnScreen(res.data[i])
+          }
+       })
+       .catch(err => console.log(err))
+       
+     
+ 
+    //AXIOS CALL FOR POST TO SAVE USER DETAILS ON CRUD CRUD SERVER
     axios
     .post("https://crudcrud.com/api/9dc38fe5d920496db56c62c49eb6370d/AppointmentDetails",myobj)
-    .then(res => showOutput(res))
+    .then(res => console.log(res))
     .catch(err => console.log(err))
+    // create a show users on screen function
+
+    function showUserOnScreen(myobj){
     const newli = document.createElement('li');
-    const newlit = document.createTextNode(res.username);
+    const newlit = document.createTextNode(myobj.username+'-'+myobj.email+'-'+myobj.phone);
     newli.appendChild(newlit);
     const button = document.createElement('button');
     const buttont = document.createTextNode('delete');
@@ -31,12 +48,14 @@ function handleFormSubmit(event){
     edit.appendChild(editt);
     edit.onclick = () =>{
    //   localStorage.removeItem(email);
-      details.removeChild(newli);
+      //details.removeChild(newli);
       document.getElementById('username').value = name;
       document.getElementById('email').value = email;
       document.getElementById('phone').value =phone;
     }
     newli.appendChild(edit);
     details.appendChild(newli);
-    
+    } 
   }
+  
+  
